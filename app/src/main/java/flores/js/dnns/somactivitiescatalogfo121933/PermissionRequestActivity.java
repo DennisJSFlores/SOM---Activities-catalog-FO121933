3916e44265990628ed61403c927e3807fe6aed7f
Dennis J Flores <dnnsflores@gmail.com>
2018-09-06 15:38:27 -0600

    Permission Request

--- app/src/main/java/flores/js/dnns/somactivitiescatalogfo121933/PermissionRequestActivity.java
+++ app/src/main/java/flores/js/dnns/somactivitiescatalogfo121933/PermissionRequestActivity.java
@@ -1,7 +1,13 @@
 package flores.js.dnns.somactivitiescatalogfo121933;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class PermissionRequestActivity extends AppCompatActivity {
 
@@ -9,5 +15,46 @@ public class PermissionRequestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission_request);
+
+        // Assume thisActivity is the current activity
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        //runtimePermission();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED){
+            Log.d("LifeCycle","Permiso dado");
+
+
+
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            Log.d("LifeCycle","Permiso no dado");
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+
+
+        }
+
+    }
+
+    private boolean runtimePermission() {
+
+        if(Build.VERSION.SDK_INT >23 && ContextCompat.checkSelfPermission(this, Manifest.permission
+                .READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission
+                (this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            Log.d("LifeCycle","Permiso no dado");
+
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission
+                    .READ_EXTERNAL_STORAGE}, 100);
+
+            return true;
+        }
+
+        return false;
     }
 }
